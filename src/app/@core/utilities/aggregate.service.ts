c8d8dda7238455b609680fc6b2baa176658484cb
Karcan Ozbal <karcanozbal@hotmail.com.tr>
2021-04-24 01:45:56 +0300

    comment added :)

--- src/app/@core/utilities/aggregate.service.ts
+++ src/app/@core/utilities/aggregate.service.ts
@@ -11,29 +11,34 @@ export class AggregateService{
     });
   }
 
-  min(array:any[]):any{
+  min(array:any[]):number{
     return array.reduce((a,b)=>((b || 0) > (a || 0)) ? a : (b), array[0]);
   }
-  max(array:any[]):any{
+  max(array:any[]):number{
     return array.reduce((a,b)=>((b || 0) < (a || 0)) ? a : (b), array[0]);
   }
-  sum(array:any[]):any{
+  sum(array:any[]):number{
     return array.reduce((a, b) => a + (b || 0), 0);
   }
-  avg(array:any[]):any{
+  avg(array:any[]):number{
     return array.reduce((a, b) => a + (b || 0), 0) / array.length;
   }
 
-  minByProperty(array:any[],propertyName:string):any{
-    return this.min(this.toArrayByProperty(array,propertyName));
+  /**
+     * @param arrayOfObjects @example [{number1: 5, number2: 10},{number1: 10, number2: 25}]
+     * @param propertyName @example 'number1'
+     * @returns
+   */
+  minByProperty(arrayOfObjects:any[],propertyName:string):number{
+    return this.min(this.toArrayByProperty(arrayOfObjects,propertyName));
   }
-  maxByProperty(array:any[],propertyName:string):any{
-    return this.max(this.toArrayByProperty(array,propertyName));
+  maxByProperty(arrayOfObjects:any[],propertyName:string):number{
+    return this.max(this.toArrayByProperty(arrayOfObjects,propertyName));
   }
-  sumByProperty(array:any[],propertyName:string):any{
-    return this.sum(this.toArrayByProperty(array,propertyName));
+  sumByProperty(arrayOfObjects:any[],propertyName:string):number{
+    return this.sum(this.toArrayByProperty(arrayOfObjects,propertyName));
   }
-  avgByProperty(array:any[],propertyName:string):any{
-    return this.avg(this.toArrayByProperty(array,propertyName));
+  avgByProperty(arrayOfObjects:any[],propertyName:string):number{
+    return this.avg(this.toArrayByProperty(arrayOfObjects,propertyName));
   }
 }
